dbef10bd22da06b96af9fdb61bfb89c93f2f3965
irixpw <59579348+irixpw@users.noreply.github.com>
2020-02-11 18:40:15 +0200

    Create test.sv

---
+++ test.sv
@@ -0,0 +1,14 @@
+#this is comment
+define version = "1" #in-line comment
+define name = "sava"
+define list = | 3,2,1 |
+
+
+print("program started")
+
+
+loop %list %i:
+    print(%i + "~~~")
+    print("-")
+
+print("exit without errors")
